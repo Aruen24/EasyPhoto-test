740bf1e1115fd12086f286e5ec58a560fe83874c
Aruen24 <wang_911205@163.com>
2023-09-28 10:45:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from easyphoto.easyphoto_utils import reload_javascript
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]='0'
 # 下载的模型都会默认下载到你设置的目录
-# os.environ['MODELSCOPE_CACHE']='./facechain_models'
+# os.environ['MODELSCOPE_CACHE']='./easyphoto_models'
 os.environ['MODELSCOPE_CACHE']='G:\\easyphoto_models'
 
 if __name__ == "__main__": 
     reload_javascript()
     easyphoto = on_ui_tabs()
     easyphoto.queue(status_update_rate=1).launch()
+    # linux
+    # easyphoto.queue(status_update_rate=1).launch(server_name="IP", share=True)
